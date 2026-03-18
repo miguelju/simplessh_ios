1b14b102472b6098aededb55a00f59a2b35f0ddb
Miguel Jackson <miguel.jackson@gmail.com>
2026-03-18 18:43:37 -0500

    Initial Commit

---
+++ simplessh/simplesshApp.swift
@@ -0,0 +1,32 @@
+//
+//  simplesshApp.swift
+//  simplessh
+//
+//  Created by Miguel Jackson on 3/18/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct simplesshApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
